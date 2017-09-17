22ddb6c0a3f4201a661b092e133fc373ba762400
Ramiro Guerreiro <ramiro.guerreiro@yahoo.ca>
2017-09-17 15:53:24 -0400

    Test de Git

---
+++ TestGit/src/testGit/TestGit.java
@@ -0,0 +1,15 @@
+package testGit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
+		
+		System.out.println("Encore une autre modification");
+
+	}
+
+}
